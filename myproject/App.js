2cd019e1720fdd4e5c9b819461c830d68e52a38d
sukibeww <sukialiong@gmail.com>
2020-02-07 20:10:57 +1100

    Played around with Touchable

--- myproject/App.js
+++ myproject/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, ScrollView, FlatList} from 'react-native';
+import { StyleSheet, Text, View, FlatList, TouchableOpacity} from 'react-native';
 
 export default function App() {
   const [ people, setPeople ] = useState([
@@ -12,6 +12,10 @@ export default function App() {
     {name: 'saki', id: "7"},
     {name: 'stan', id: "8"},
   ])
+
+  const pressHandler = (id) =>{
+    setPeople(()=> {return people.filter((item) => item.id != id)})
+  }
   return (
     <View style={styles.container}>
       <FlatList
@@ -19,7 +23,9 @@ export default function App() {
         keyExtractor={(item) => item.id}
         data={people}
         renderItem={({item}) => (
-          <Text style={styles.item}>{item.name}</Text>
+          <TouchableOpacity onPress={()=> pressHandler(item.id)}>
+            <Text style={styles.item}>{item.name}</Text>
+          </TouchableOpacity>
         )}
       />
       {/* <ScrollView>
